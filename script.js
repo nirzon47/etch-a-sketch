9086d02479a5372f1837eaead9f6b91f9275c814
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-02-24 23:26:55 +0530

    added hover function

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ const container = document.getElementById('container')
 for (let index = 0; index < 256; index++) {
 	const item = document.createElement('div')
 	item.className = 'item'
+
+	item.addEventListener('mouseover', mouseOver)
+
+	function mouseOver() {
+		item.classList.add('active')
+	}
+
 	container.append(item)
 }
+
+const item = document.getElementsByClassName('item')
