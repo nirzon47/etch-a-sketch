162649eb9e3cda6c017e505a2325960cde73d30c
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-02-26 18:56:19 +0530

    added basic custom grid size and reset functions

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const container = document.getElementById('container')
 
-for (let index = 0; index < 256; index++) {
-	const item = document.createElement('div')
-	item.className = 'item'
+function generateNewSketch() {
+	const gridSize = prompt('Enter new grid size (Max input: 100, Min input: 1')
+	removeAllChildren()
 
-	item.addEventListener('mouseover', mouseOver)
-
-	function mouseOver() {
-		item.classList.add('active')
+	if (gridSize > 100) {
+		alert('Grid size too big, try to limit input to 100')
+		return
+	} else if (gridSize < 1) {
+		alert('The input is less than 1, minimum grid size is 1')
+		return
+	} else if (!Number(gridSize)) {
+		alert('The input is not a number, try again')
+		return
 	}
 
-	container.append(item)
+	for (let index = 0; index < gridSize * gridSize; index++) {
+		const item = document.createElement('div')
+		item.className = 'item'
+
+		item.style.width = `${50 / gridSize}vh`
+		item.style.height = `${50 / gridSize}vh`
+
+		item.addEventListener('mouseover', mouseOver)
+
+		function mouseOver() {
+			item.classList.add('active')
+		}
+
+		container.append(item)
+	}
 }
 
-const item = document.getElementsByClassName('item')
+function reset() {
+	let item = document.getElementsByClassName('item')
+
+	for (let i = 0; i < item.length; i++) {
+		item[i].classList.remove('active')
+	}
+}
+
+function removeAllChildren() {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild)
+	}
+}
